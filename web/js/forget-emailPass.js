f87c6d36e3d5b8b49fc6223299f6d81f2569044c
ambujbpl <ambujdubey89@gmail.com>
2017-11-03 14:38:36 +0530

    update forget password

--- web/js/forget-emailPass.js
+++ web/js/forget-emailPass.js
@@ -20,7 +20,7 @@ $.validator.setDefaults({
     //     if (res.resCode == 'OK') {
     //       console.log(res.msg);
     //       if (res.msg != "user not found create new user") {
-    //         passwordValidationScript();
+    //         paswordValidationScript();
     //         window.location.replace("forgot-Password.html");
     //       }
     //     } else {
@@ -31,22 +31,11 @@ $.validator.setDefaults({
     //     console.log("fail login");
     //   }
     // });
-    $('#divEmailNum').addClass('hide');
-    $('.divPass').removeClass('hide');
-    $("#emailvalid").attr("id", "passwordchange");
-    passwordValidationScript();
+    window.location.replace("forgot-Password.html");
   }
 });
 
 $(document).ready(function() {
-  //Start---  for change password script
-  var my_param = window.location.href;
-  // console.log(my_param);
-  key = my_param.split('?').pop();
-  if (key === "key") {
-    $('#otpdiv').addClass('hide');
-  }
-  //end---  for change password script
   $('.img').addClass('hide');
   $('.img1').addClass('hide');
   $('#password').hover(function() {
@@ -103,80 +92,21 @@ $(document).ready(function() {
 
   });
 });
-
 function mDown() {
-  password.type = 'text';
+    password.type='text';
 }
 
 function mUp() {
-  password.type = 'password';
+    password.type='password';
 }
-
 function mDown1() {
-  Confirm_Password.type = 'text';
+    Confirm_Password.type='text';
 }
 
 function mUp1() {
-  Confirm_Password.type = 'password';
+    Confirm_Password.type='password';
 }
 
-function passwordValidationScript() {
-  // alert("passwordValidationScript();");
-  $.validator.addMethod("pwcheckspechars", function(value) {
-    return /[!@#$%^&*()_=\[\]{};':"\\|,.<>\/?+-]/.test(value)
-  });
-  $.validator.addMethod("pwchecknumber", function(value) {
-    return /\d/.test(value) // has a digit
-  });
+function paswordValidationScript(){
 
-  var confirm = document.getElementById("Confirm_Password").value;
-  $("#passwordchange").validate({
-    rules: {
-      password: {
-        required: true,
-        minlength: 5,
-        maxlength: 12,
-        pwcheckspechars: true,
-        pwchecknumber: true
-      },
-      Confirm_Password: {
-        required: true,
-        minlength: 5,
-        equalTo: "#password"
-      },
-
-    },
-    messages: {
-      password: {
-        required: "Please provide a password",
-        minlength: "Your password must be at least 5 characters long",
-        manlength: "Your password must be at most 12 characters long",
-        pwchecknumber: "The password must contain at least one number",
-        pwcheckspechars: "at 1 Special Character required"
-      },
-      Confirm_Password: {
-        required: "Please provide a password",
-        minlength: "Your password must be at least 5 characters long",
-        equalTo: "Please enter the same password as above"
-      },
-
-    },
-    errorElement: "em",
-    errorPlacement: function(error, element) {
-      // Add the `help-block` class to the error element
-      error.addClass("help-block");
-
-      if (element.prop("type") === "checkbox") {
-        error.insertAfter(element.parent("label"));
-      } else {
-        error.insertAfter(element);
-      }
-    },
-    highlight: function(element, errorClass, validClass) {
-      $(element).parents(".col-sm-5").addClass("has-error").removeClass("has-success");
-    },
-    unhighlight: function(element, errorClass, validClass) {
-      $(element).parents(".col-sm-5").addClass("has-success").removeClass("has-error");
-    }
-  });
 };
