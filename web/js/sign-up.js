8455cb674f9710bd72d2bc487f368b7242ebc827
ambujbpl <ambujdubey89@gmail.com>
2017-11-23 14:24:37 +0530

    Removed Static html form for all role in the file sign-up.html

--- web/js/sign-up.js
+++ web/js/sign-up.js
@@ -1,25 +1,15 @@
-// A $( document ).ready() block.
 $(document).ready(function() {
-  $('input:radio[name=inlineRadioOptions][value=s]').click();
-  var date_input = $('input[name="date"]'); //our date input has the name "date"
-  var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
-  var options = {
-    format: 'dd/mm/yyyy',
-    container: container,
-    todayHighlight: true,
-    autoclose: true,
-  };
-  date_input.datepicker(options);
+  // $("#containerload").load("studentSign-up.html");
 });
 $('input:radio[name="inlineRadioOptions"]').change(function() {
   if ($(this).val() == 's') {
-    $('.containerCssForm').addClass('hide');
-    $('#formStudent').removeClass('hide');
+    $("#containerload").load("studentSign-up.html");
     //  alert("You Picked as a Student in Signup Form");
     $('#studentName').focus().select();
     $('.headerSpan1').html('Student Registration Form');
-    $('.registrationSpan').html("For Student Registration amount is 100 Rupees. Please check for fee submission.");
-  } else if ($(this).val() == 'i') {
+    // $('.registrationSpan').html("For Student Registration amount is 100 Rupees. Please check for fee submission.");
+    } else if ($(this).val() == 'i') {
+    $("#containerload").load("itiSign-up.html");
     $('.containerCssForm').addClass('hide');
     $('#formITI').removeClass('hide');
     // alert("You Picked as a ITI Institute in Signup Form");
@@ -27,6 +17,7 @@ $('input:radio[name="inlineRadioOptions"]').change(function() {
     $('.headerSpan1').html('ITI Institute Registration Form');
     $('.registrationSpan').html("For Institute Registration amount is 500 Rupees. Please check for fee submission.");
   } else if ($(this).val() == 'c') {
+    $("#containerload").load("companySign-up.html");
     $('.containerCssForm').addClass('hide');
     $('#formCompany').removeClass('hide');
     // alert("You Picked as a Company in Signup Form");
@@ -35,12 +26,16 @@ $('input:radio[name="inlineRadioOptions"]').change(function() {
     $('.registrationSpan').html("For Company Registration amount is 1000 Rupees. Please check for fee submission.");
 
   } else if ($(this).val() == 'a') {
+    $("#containerload").load("adminSign-up.html");
     $('.containerCssForm').addClass('hide');
     $('#formAdmin').removeClass('hide');
     // alert("You Picked as a Admin in Signup Form");
     $('#adminName').focus().select();
     $('.headerSpan1').html('Admin Registration Form');
-    $('.registrationSpan').html("Please check for final submission.");
-
   }
 });
+var login = storagegetItem("login");
+console.log(login);
+if((login == null)||(login == "")||(login == undefined)){
+  $('.adminRedioDiv').addClass('hide');
+}
